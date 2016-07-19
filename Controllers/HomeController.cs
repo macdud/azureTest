e6feba32e7d19a13850101ab5a3ec6445e056209
Maciej Dudzinski <mdudzi90@gmail.com>
2016-07-19 17:06:23 +0100

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using TravelAssistant.Models;
+
+namespace TravelAssistant.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var url = "https://www.eventbriteapi.com/v3/events/search/?location.address=SW15+4BZ&location.within=1mi&token=C3L6OXM4V3DVDEW5FNXN";
+            var resultString = getJSON(url);
+            var result = JsonConvert.DeserializeObject<EventViewModel>(resultString);
+
+            return View(result);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        string getJSON(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+    }
+}
---
+++ Models/EventModels.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelAssistant.Models
+{
+    public class EventViewModel
+    {
+        public Pagination pagination { get; set; }
+        public List<Event> events { get; set; }
+        public Location location { get; set; }
+    }
+
+    public class Pagination
+    {
+        public int object_count { get; set; }
+        public int page_number { get; set; }
+        public int page_size { get; set; }
+        public int page_count { get; set; }
+    }
+
+    public class Name
+    {
+        public string text { get; set; }
+        public string html { get; set; }
+    }
+
+    public class Description
+    {
+        public string text { get; set; }
+        public string html { get; set; }
+    }
+
+    public class Start
+    {
+        public string timezone { get; set; }
+        public string local { get; set; }
+        public string utc { get; set; }
+    }
+
+    public class End
+    {
+        public string timezone { get; set; }
+        public string local { get; set; }
+        public string utc { get; set; }
+    }
+
+    public class Logo
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public string aspect_ratio { get; set; }
+        public string edge_color { get; set; }
+        public bool edge_color_set { get; set; }
+    }
+
+    public class Event
+    {
+        public Name name { get; set; }
+        public Description description { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string vanity_url { get; set; }
+        public Start start { get; set; }
+        public End end { get; set; }
+        public string created { get; set; }
+        public string changed { get; set; }
+        public int capacity { get; set; }
+        public string status { get; set; }
+        public string currency { get; set; }
+        public bool listed { get; set; }
+        public bool shareable { get; set; }
+        public bool online_event { get; set; }
+        public int tx_time_limit { get; set; }
+        public bool hide_start_date { get; set; }
+        public bool hide_end_date { get; set; }
+        public string locale { get; set; }
+        public bool is_locked { get; set; }
+        public string privacy_setting { get; set; }
+        public bool is_series { get; set; }
+        public bool is_series_parent { get; set; }
+        public bool is_reserved_seating { get; set; }
+        public string logo_id { get; set; }
+        public string organizer_id { get; set; }
+        public string venue_id { get; set; }
+        public string category_id { get; set; }
+        public string subcategory_id { get; set; }
+        public string format_id { get; set; }
+        public string resource_uri { get; set; }
+        public Logo logo { get; set; }
+        public string series_id { get; set; }
+    }
+
+    public class Location
+    {
+        public string latitude { get; set; }
+        public string within { get; set; }
+        public string longitude { get; set; }
+        public string address { get; set; }
+    }
+
+
+}
